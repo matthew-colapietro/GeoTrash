49d474e1a15e5aac1f7fb95d62d0a5879dd64d58
MCola <mcolapietro502@gmail.com>
2020-07-18 14:01:13 -0400

    round 2 of heroku deployment

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const http = require("http");
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
@@ -18,6 +19,21 @@ app.use(bodyParser.urlencoded({
 
 app.use(mainRoutes)
 
-app.listen(8000, () => {
-  console.log('Node.js listening on port ' + 8000)
-})
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 8000;
+const server = http.createServer(app);
+
+server.listen(port);
+console.log("Server listening on:", port);
